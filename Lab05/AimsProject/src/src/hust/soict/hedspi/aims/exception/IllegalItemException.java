266bd2c7dbeceb2060503452e8ecc78ade74d35a
Thiennguyen113 <nguyenquangthien1132005@gmail.com>
2025-05-30 01:44:41 +0700

    Delete Lab05 directory

--- Lab05/AimsProject/src/src/hust/soict/hedspi/aims/exception/IllegalItemException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.hedspi.aims.exception;
-
-public class IllegalItemException extends Exception {
-    public IllegalItemException(String message) {
-        super(message);
-    }
-}
